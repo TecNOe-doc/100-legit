1e6a860e48885ab49f18d93ba64b32be30924b79
TecNOe-doc <vrankicluka@gmail.com>
2020-02-24 17:16:40 +0100

    'something'

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+// ez boi scam
+alert('Free knife');
+const hours = 90
+// some bullshit
+console.log(hours);
+console.log('That' + ' ' + 'takes' + ' ' + 'too' + ' ' + 'long');
+ 
+/* trying something new
+because this works in  few ways */
+
+const knife = 500
+const value = 2
+console.log(value + knife + ' ' + 'thats too much for a virtual game');
+ 
+// little library commands
+
+const riba = 'acervatio'
+console.log(riba.lenght)
+
+let sarma = 'zid'
+console.log(sarma.toUpperCase)
+
+console.log(Math.random())
+
+const num1 = 5
+const num2 = 10
+function sum(num1, num2){
+    return num1 * num2
+}
+
+//little flex hehe boi
+
+
+
+
